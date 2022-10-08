ec77223911744bd65c7c7ff3d11ed9f5022b0de1
Igor Kompanets <kombainer86@gmail.com>
2022-10-08 11:35:37 +0300

    .gitignore added. Task1 done

---
+++ Task1/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int a = 1;
+while (a<=N)
+{
+    int b = a*a*a;                          //Math.Pow(a,3);
+    Console.WriteLine($"Куб числа {a} равен {b}");
+    a++;
+}
