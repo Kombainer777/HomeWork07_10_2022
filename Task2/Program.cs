a2bde70c52e1dfd2a9d1763bb28ddcf832979660
Igor Kompanets <kombainer86@gmail.com>
2022-10-08 13:45:26 +0300

    Task2 completed

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Подсчитать сумму цифр в числе
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int sum = 0;
+int b = 0;
+
+    while (a>0)
+    {
+    b = a % 10;
+    sum = sum + b;
+    a = a / 10;
+    }
+Console.WriteLine($"Сумма цифр равна:  {sum}");
