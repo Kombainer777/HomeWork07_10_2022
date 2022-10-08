b6f883e0fc7348eec913f19b72693e4e03189cf8
Igor Kompanets <kombainer86@gmail.com>
2022-10-08 13:56:25 +0300

    Task 3 completed

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+int a = 1;
+int fct = 1;
+while (a <= N)
+{
+    fct = fct * a;
+    a++;
+}
+Console.WriteLine($"Произведение всех чисел от 1 до указанного числа {N} равняется {fct}");
